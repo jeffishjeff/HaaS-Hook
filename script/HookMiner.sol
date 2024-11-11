dfa3850575dbea2c57ff067fc2bd801dbc1da1f4
Qing Yi <qing.yi@outlook.com>
2024-11-11 18:01:09 +0800

    fix wrong mask in HookMiner.

--- script/HookMiner.sol
+++ script/HookMiner.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 /// @title HookMiner - a library for mining hook addresses
 /// @dev This library is intended for `forge test` environments. There may be gotchas when using salts in `forge script` or `forge create`
 library HookMiner {
+    uint160 private constant MASK = 0x3FFF;
+
     /// @notice Find a salt that produces a hook address with the desired `flags`
     /// @param deployer The address that will deploy the hook. In `forge test`, this will be the test contract `address(this)` or the pranking address
     ///                 In `forge script`, this should be `0x4e59b44847b379578588920cA78FbF26c0B4956C` (CREATE2 Deployer Proxy)
@@ -21,7 +23,7 @@ library HookMiner {
             hookAddress = address(uint160(uint256(keccak256(toHash))));
 
             assembly {
-                valid := eq(and(hookAddress, flags), flags)
+                valid := eq(and(hookAddress, MASK), flags)
             }
             if (valid) {
                 break;
